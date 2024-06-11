8f0e9f4c982b1a00217b07392ff8f5a42598a668
Alexander Drach <alexander-drach@mail.ru>
2024-06-11 21:45:46 +0600

    add mobile menu

--- assets-gulp/source/js/modules/mobile-menu/menu.js
+++ assets-gulp/source/js/modules/mobile-menu/menu.js
@@ -2,11 +2,19 @@ const openMenu = () => {
   const hamb = document.querySelector('.hamb');
   const header = document.querySelector('.header');
   const body = document.querySelector('body');
+  const mobileMenuItems = document.querySelectorAll('.header .nav a');
   
   hamb.addEventListener('click', () => {
     header.classList.toggle('open');
     body.classList.toggle('open');
   });
+
+  mobileMenuItems.forEach((item) => {
+      item.addEventListener('click', () => {
+      header.classList.remove('open');
+      body.classList.remove('open');
+    })
+  })
 };
 
 export {openMenu};
